50eee7d2f6b6612f732a30744636a01a0ea6aaee
Jillian Fussner <jillian.fussner@gmail.com>
2023-09-06 19:47:31 -0500

    brackets corrected

--- unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
+++ unit-testing/studio/balanced-brackets/src/test/java/org/launchcode/BalancedBracketsTest.java
@@ -10,4 +10,45 @@ class BalancedBracketsTest {
     public void emptyTest() {
         assertEquals(true, true);
     }
+
+    @Test
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+    @Test
+    public void inbalancedBracketsReturnsFalse() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[]"));
+    }
+    @Test
+    public void closingBracketStarterFails() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("][]"));
+    }
+    @Test
+    public void noBracketsPasses() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("test"));
+    }
+    @Test
+    public void extraClosingBracketFails() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]][]]][[[]"));
+    }
+    @Test
+    public void nestedBracketsPass() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[[[[]]]]]"));
+    }
+    @Test
+    public void mixedNestsPass() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[[][][[[]]]]"));
+    }
+    @Test
+    public void nestedExtraClosingBracketsFails() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]][[]"));
+    }
+    @Test
+    public void noClosingBracketFails() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]["));
+    }
+    @Test
+    public void codeWithTextPasses() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[LaunchCode]"));
+    }
 }
